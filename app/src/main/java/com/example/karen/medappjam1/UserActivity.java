ae025d2357cefded8ce17fb049a9b89225f3b485
Richmond <richmonl@uci.edu>
2016-11-19 20:37:34 -0800

    moved forum activity to home screen

--- app/src/main/java/com/example/karen/medappjam1/UserActivity.java
+++ app/src/main/java/com/example/karen/medappjam1/UserActivity.java
@@ -1,18 +1,32 @@
 package com.example.karen.medappjam1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         Intent intent = getIntent();
         String user = intent.getStringExtra("username");
+
+        button = (Button) findViewById(R.id.buttonUrl);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
