d97d75c166a6318ab254223b867ac992d8a4e5db
Karen <panteat@uci.edu>
2016-11-21 14:30:18 -0800

    attempting to merge everything

---
+++ app/src/main/java/com/example/karen/medappjam1/add_event.java
@@ -0,0 +1,13 @@
+package com.example.karen.medappjam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
